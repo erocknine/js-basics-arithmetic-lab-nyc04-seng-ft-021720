3329f805211eff6d2b3a6bf5cabef1bac5c6741b
Sir Eric <echeungsk@gmail.com>
2020-03-28 20:06:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // Write your code here
+let two = 2;
+let thirtyOne = 31;
+let eight = 8;
+let twelve = 12
 
-multiply = 2 * 31;
+let multiply = two * thirtyOne;
+let random = Math.random();
+let mod = eight * two % twelve;
+let max = Math.max(1, 5, 10, 20)
